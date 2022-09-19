bf34404fdb3cab9e2d5f5f2add7f22683c595009
GCZ-Zero <1203970284@qq.com>
2022-09-19 13:57:19 +0000

    Update 6 files

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HelloWorld from "../components/HelloWorld.vue";
+import Home from "../views/Home.vue";
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: "/",
-      component: HelloWorld
+      component: Home
     }
   ]
 });
